8675c8e367539ff690e28ed4008c2270d21aaab2
Alexandru Puiu <apuiu@transperfect.com>
2019-12-27 23:40:50 -0500

    Created VS solution and added editorconfig, pipeline definition and a blank readme file Added MerkeTree implementation made by Marc Clifton Added first draft of interfaces for audit blockchain

---
+++ Blockchain/IAuditEntry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Blockchain
+{
+    public interface IAuditEntry
+    {
+        /// <summary>
+        /// Identifier of subject accessing the resource
+        /// </summary>
+        string Subject { get; set; }
+
+        /// <summary>
+        /// Cross-system correlation id which can be used to cross-reference logs in other systems or trace request
+        /// </summary>
+        string CorrelationId { get; set; }
+
+        /// <summary>
+        /// Date the authorization decision was made
+        /// </summary>
+        DateTime DecisionDate { get; set; }
+
+        /// <summary>
+        /// Authorization decision made
+        /// </summary>
+        AuthorizationDecisions AuthorizationDecision { get; set; }
+
+        /// <summary>
+        /// Identifier of resolver which made the authorization decision
+        /// </summary>
+        string Resolver { get; set; }
+
+        /// <summary>
+        /// Id/name of policy that dictated this requirement
+        /// </summary>
+        string Policy { get; set; }
+
+        /// <summary>
+        /// Unique Id of resource accessed (Ex: file path, document id, record number, url)
+        /// </summary>
+        string ResourceId { get; set; }
+
+        /// <summary>
+        /// Compute hash of audit entry
+        /// </summary>
+        /// <returns></returns>
+        string CalculateAuditEntryHash();
+    }
+}
---
+++ Blockchain/IBlockChain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blockchain
+{
+    public interface IBlockChain
+    {
+        /// <summary>
+        /// Accept a block
+        /// </summary>
+        /// <param name="block"></param>
+        void AcceptBlock(IBlock block);
+
+        /// <summary>
+        /// Next block sequence number
+        /// </summary>
+        long NextBlockNumber { get; }
+
+        /// <summary>
+        /// Verify the full chain
+        /// </summary>
+        void VerifyChain();
+    }
+}
