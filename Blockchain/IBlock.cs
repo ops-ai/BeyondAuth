8b63d6790917ee66e08135af94b67940657b6fb3
Alexandru Puiu <apuiu@transperfect.com>
2019-12-27 23:40:50 -0500

    Created VS solution and added editorconfig, pipeline definition and a blank readme file Added MerkeTree implementation made by Marc Clifton Added first draft of interfaces for audit blockchain

---
+++ Blockchain/IBlock.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blockchain
+{
+    public interface IBlock
+    {
+        /// <summary>
+        /// List of authorization decisions made
+        /// </summary>
+        List<IAuditEntry> AuditEntries { get; }
+
+        // Block header data
+        /// <summary>
+        /// Block sequence number
+        /// </summary>
+        int BlockNumber { get; }
+
+        /// <summary>
+        /// Date the block was created
+        /// </summary>
+        DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Current block hash
+        /// </summary>
+        string BlockHash { get; }
+
+        /// <summary>
+        /// Hash of previous block in chain
+        /// </summary>
+        string PreviousBlockHash { get; set; }
+
+
+        /// <summary>
+        /// Record a new authorization decision
+        /// </summary>
+        /// <param name="auditEntry"></param>
+        void AddAuditEntry(IAuditEntry auditEntry);
+
+        /// <summary>
+        /// Compute hash of current block
+        /// </summary>
+        /// <param name="previousBlockHash">Hash of previous block</param>
+        /// <returns></returns>
+        string CalculateBlockHash(string previousBlockHash);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="previousBlock"></param>
+        void SetBlockHash(IBlock previousBlock);
+
+        /// <summary>
+        /// Reference to next block
+        /// </summary>
+        IBlock NextBlock { get; set; }
+
+        /// <summary>
+        /// Check if the previous block's hash is valid
+        /// </summary>
+        /// <param name="prevBlockHash"></param>
+        /// <returns></returns>
+        bool IsValidChain(string prevBlockHash);
+
+        /// <summary>
+        /// Abstracted key store to allow interop with HSMs and KMS'
+        /// </summary>
+        IKeyStore KeyStore { get; }
+    }
+}
---
+++ Blockchain/IKeyStore.cs
@@ -0,0 +1,25 @@
+﻿namespace Blockchain
+{
+    public interface IKeyStore
+    {
+        /// <summary>
+        /// HMAC key
+        /// </summary>
+        byte[] AuthenticatedHashKey { get; }
+
+        /// <summary>
+        /// Sign a block
+        /// </summary>
+        /// <param name="blockHash">The block's hash</param>
+        /// <returns></returns>
+        string SignBlock(string blockHash);
+
+        /// <summary>
+        /// Verify a block
+        /// </summary>
+        /// <param name="blockHash">The block's hash</param>
+        /// <param name="signature">Signature</param>
+        /// <returns></returns>
+        bool VerifyBlock(string blockHash, string signature);
+    }
+}
