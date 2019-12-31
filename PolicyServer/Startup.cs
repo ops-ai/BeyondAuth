fca82743025d1a3492e82dc15b4d12fb318b3c2a
Alexandru Puiu <alex@ops.ai>
2019-12-30 22:32:49 -0500

    Added RavenDB to policy server Added JWT authentication support Added CorrelationId, reverse proxy middleware, security headers Added health checks for dependencies Added autofac

--- PolicyServer/Startup.cs
+++ PolicyServer/Startup.cs
@@ -1,15 +1,20 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Autofac;
+using Autofac.Configuration;
+using CorrelationId.DependencyInjection;
+using HealthChecks.UI.Client;
+using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Raven.Client.Documents;
 
 namespace PolicyServer
 {
@@ -22,10 +27,59 @@ namespace PolicyServer
 
         public IConfiguration Configuration { get; }
 
+        public ILifetimeScope AutofacContainer { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardLimit = 2;
+                Configuration["ProxyNodes"]?.Split(';').ToList().ForEach(t =>
+                {
+                    if (!string.IsNullOrEmpty(t))
+                        options.KnownProxies.Add(IPAddress.Parse(t));
+                });
+            });
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(x =>
+                {
+                    x.Authority = Configuration["Authentication:Authority"];
+                    x.ApiSecret = Configuration["Authentication:ApiSecret"];
+                    x.ApiName = Configuration["Authentication:ApiName"];
+                    x.SupportedTokens = SupportedTokens.Both;
+                    x.RequireHttpsMetadata = true;
+                    x.EnableCaching = true;
+                    x.CacheDuration = TimeSpan.FromMinutes(1);
+                    x.NameClaimType = "sub";
+                });
+
+            services.AddCorrelationId();
+
+            services.AddSingleton((ctx) => new DocumentStore
+            {
+                Urls = new[] { Configuration["Raven:Url"] },
+                Database = Configuration["Raven:Database"],
+                Certificate = Configuration.GetSection("Raven:EncryptionEnabled").Get<bool>() ? new X509Certificate2(Configuration["Raven:CertFile"], Configuration["Raven:CertPassword"]) : null
+            }.Initialize());
+
+            services.AddHealthChecks()
+                .AddRavenDB(setup => { setup.Urls = new[] { Configuration["Raven:Url"] }; setup.Database = Configuration["Raven:Database"]; setup.Certificate = Configuration.GetSection("Raven:EncryptionEnabled").Get<bool>() ? new X509Certificate2(Configuration["Raven:CertFile"], Configuration["Raven:CertPassword"]) : null; }, "ravendb");
+
             services.AddControllers();
+        }
+
+        /// <summary>
+        /// ConfigureContainer is where you can register things directly
+        /// with Autofac. This runs after ConfigureServices so the things
+        /// here will override registrations made in ConfigureServices.
+        /// </summary>
+        /// <param name="builder"></param>
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            var module = new ConfigurationModule(Configuration);
+            builder.RegisterModule(module);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,9 +92,30 @@ namespace PolicyServer
 
             app.UseHttpsRedirection();
 
+            app.UseForwardedHeaders();
+
+            app.UseCors(x => x.AllowAnyOrigin().WithHeaders("accept", "authorization", "content-type", "origin").AllowAnyMethod());
+
+            app.UseXContentTypeOptions();
+            app.UseXDownloadOptions();
+            app.UseXfo(options => options.SameOrigin());
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseHsts(options => options.MaxAge(30).AllResponses());
+
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseHealthChecks(Configuration["HealthChecks:FullEndpoint"], new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecks(Configuration["HealthChecks:SummaryEndpoint"], new HealthCheckOptions()
+            {
+                Predicate = _ => _.FailureStatus == HealthStatus.Unhealthy
+            });
 
             app.UseEndpoints(endpoints =>
             {
