bc9890c5a9e42bbccdb91ab3a0ebf05070ad7bb1
Alexandru Puiu <alex@ops.ai>
2019-12-30 23:30:33 -0500

    Added integration test project for Policy Server Added security headers test to make sure all security headers are set

---
+++ Tests/PolicyServer.Tests/Integration Tests/PolicyServerWebApplicationFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace PolicyServer.Tests.Integration_Tests
+{
+    public class PolicyServerWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Test")
+                .ConfigureServices(services =>
+                {
+                    services.AddMvc(options =>
+                    {
+
+                    });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
+
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
---
+++ Tests/PolicyServer.Tests/Integration Tests/SecurityHeaderTests.cs
@@ -0,0 +1,59 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PolicyServer.Tests.Integration_Tests
+{
+    public class SecurityHeaderTests : IClassFixture<PolicyServerWebApplicationFactory<Startup>>
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly HttpClient _client;
+
+        protected readonly Mock<ILoggerFactory> _loggerFactoryMock;
+
+        public SecurityHeaderTests(PolicyServerWebApplicationFactory<Startup> factory, ITestOutputHelper output)
+        {
+            _output = output;
+
+            _client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IHealthCheck));
+                    if (descriptor != null)
+                    {
+                        services.Remove(descriptor);
+                    }
+                });
+
+                builder.ConfigureTestContainer<ContainerBuilder>(services =>
+                {
+                    
+                });
+            }).CreateClient();
+        }
+
+        [Fact(DisplayName = "Security headers are present")]
+        public async Task ServerHeadersPresent()
+        {
+            var response = await _client.GetAsync("policies");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.Contains(response.Headers, t => t.Key == "X-Content-Type-Options");
+            Assert.Contains(response.Headers, t => t.Key == "X-Download-Options");
+            Assert.Contains(response.Headers, t => t.Key == "X-Frame-Options");
+            Assert.Contains(response.Headers, t => t.Key == "X-XSS-Protection");
+            Assert.Contains(response.Headers, t => t.Key == "Strict-Transport-Security");
+        }
+    }
+}
