2149b6cb778acd8f299e5c9783c770a3247b29f8
Alexandru Puiu <alex@ops.ai>
2020-01-02 23:59:01 -0500

    Added implementations for ravendb identityserver stores for resources and clients

---
+++ IdentityServer4.Contrib.RavenDB/Stores/ClientStore.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Stores;
+using Microsoft.Extensions.Logging;
+using Raven.Client.Documents;
+using System.Threading.Tasks;
+
+namespace IdentityServer4.Contrib.RavenDB.Stores
+{
+    public class ClientStore : IClientStore
+    {
+        private readonly ILogger _logger;
+        private readonly IDocumentStore _store;
+
+        public ClientStore(ILoggerFactory loggerFactory, IDocumentStore store)
+        {
+            _logger = loggerFactory.CreateLogger<ClientStore>();
+            _store = store;
+        }
+
+        public async Task<Client> FindClientByIdAsync(string clientId)
+        {
+            using (var session = _store.OpenAsyncSession())
+            {
+                _logger.LogDebug($"Loading client {clientId} from document store");
+                return await session.LoadAsync<Client>(clientId);
+            }
+        }
+    }
+}
