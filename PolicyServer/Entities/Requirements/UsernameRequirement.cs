baf595ef3072671183eb5c7dc0835fddb38875d5
Alexandru Puiu <alex@ops.ai>
2020-01-04 23:02:05 -0500

    Added claim, group membership, role, scopes and usernames requirements

---
+++ PolicyServer/Entities/Requirements/UsernameRequirement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PolicyServer.Entities.Requirements
+{
+    /// <summary>
+    /// Username requirement
+    /// </summary>
+    public class UsernameRequirement : AuthorizationRequirement
+    {
+        /// <summary>
+        /// Unique name of requirement
+        /// </summary>
+        public override string Name => "username";
+
+        /// <summary>
+        /// List of usernames the user must match
+        /// </summary>
+        public List<string> Usernames { get; set; }
+    }
+}
