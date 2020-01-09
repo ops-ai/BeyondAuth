0c9a00d756b740493185ed05e6b229b7a83e2283
Alexandru Puiu <alex@ops.ai>
2020-01-08 23:58:18 -0500

    Added security headers, jwt authentication and health checks to authorization project

--- AuthorizationServer/Startup.cs
+++ AuthorizationServer/Startup.cs
@@ -1,21 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using CorrelationId.DependencyInjection;
+using HealthChecks.UI.Client;
+using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 
 namespace AuthorizationServer
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardLimit = 2;
+                Configuration["ProxyNodes"]?.Split(';').ToList().ForEach(t =>
+                {
+                    if (!string.IsNullOrEmpty(t))
+                        options.KnownProxies.Add(IPAddress.Parse(t));
+                });
+            });
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(x =>
+                {
+                    x.Authority = Configuration["Authentication:Authority"];
+                    x.ApiSecret = Configuration["Authentication:ApiSecret"];
+                    x.ApiName = Configuration["Authentication:ApiName"];
+                    x.SupportedTokens = SupportedTokens.Both;
+                    x.RequireHttpsMetadata = true;
+                    x.EnableCaching = true;
+                    x.CacheDuration = TimeSpan.FromMinutes(1);
+                    x.NameClaimType = "sub";
+                });
+
+            services.AddCorrelationId();
+
             services.AddGrpc();
         }
 
@@ -27,7 +63,22 @@ namespace AuthorizationServer
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseForwardedHeaders();
+
             app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseHealthChecks(Configuration["HealthChecks:FullEndpoint"], new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecks(Configuration["HealthChecks:SummaryEndpoint"], new HealthCheckOptions()
+            {
+                Predicate = _ => _.FailureStatus == HealthStatus.Unhealthy
+            });
 
             app.UseEndpoints(endpoints =>
             {
