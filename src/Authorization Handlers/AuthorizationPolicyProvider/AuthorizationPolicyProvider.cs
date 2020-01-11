ac277f7046e63d59afdd56ac02c0cd624d594916
Alexandru Puiu <alex@ops.ai>
2020-01-10 21:11:26 -0500

    Restructured folders Added Authorization policy provider project

---
+++ src/Authorization Handlers/AuthorizationPolicyProvider/AuthorizationPolicyProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Threading.Tasks;
+
+namespace AuthorizationPolicyProvider
+{
+    public class AuthorizationPolicyProvider : IAuthorizationPolicyProvider
+    {
+        public AuthorizationPolicyProvider()
+        {
+
+        }
+
+        public Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthorizationPolicy> GetFallbackPolicyAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
