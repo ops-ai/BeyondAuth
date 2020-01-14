b4a1550db6c87dd312228685d342aad0dd7e4fea
Alexandru Puiu <alex@ops.ai>
2020-01-13 23:53:47 -0500

    Added interface for transaction pool

---
+++ src/Blockchain/ITransactionPool.cs
@@ -0,0 +1,12 @@
+﻿namespace Blockchain
+{
+    /// <summary>
+    /// Audit entry transaction pool
+    /// </summary>
+    public interface ITransactionPool
+    {
+        void AddAuditEntry(IAuditEntry auditEntry);
+
+        IAuditEntry GetAuditEntry();
+    }
+}
