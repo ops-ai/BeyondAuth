7b85b5d16415a5f9bc91e4cbf5fda39d527dd1c3
Alexandru Puiu <alex@ops.ai>
2020-01-17 17:18:27 -0500

    Added tests for reference token store and refresh token store

---
+++ src/Tests/IdentityServer4.Contrib.RavenDB.Tests/RavenDBReferenceTokenStoreTests.cs
@@ -0,0 +1,156 @@
+using Divergic.Logging.Xunit;
+using FluentAssertions;
+using IdentityServer4.Contrib.RavenDB.Stores;
+using IdentityServer4.Contrib.RavenDB.Tests.Common;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+using Microsoft.Extensions.Logging;
+using Raven.Client.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IdentityServer4.Contrib.RavenDB.Tests
+{
+    public class RavenDBReferenceTokenStoreTests : RavenIdentityServerTestBase
+    {
+        protected readonly ILoggerFactory _loggerFactory;
+        private readonly IReferenceTokenStore _referenceTokenStore;
+        private readonly IDocumentStore _documentStore;
+
+        public RavenDBReferenceTokenStoreTests(ITestOutputHelper output)
+        {
+            _loggerFactory = LogFactory.Create(output);
+            _documentStore = GetDocumentStore();
+            _referenceTokenStore = new RavenDBReferenceTokenStore(_loggerFactory.CreateLogger<RavenDBReferenceTokenStore>(), _documentStore);
+        }
+
+        [Fact(DisplayName = "Reference token should be retrievable after storage")]
+        public async Task StoreAndGetReferenceTokenAsync()
+        {
+            var token1 = new Token()
+            {
+                ClientId = "client",
+                Audiences = { "aud" },
+                CreationTime = DateTime.UtcNow,
+                Lifetime = 10,
+                Type = "type",
+                Claims = new List<Claim>
+                    {
+                        new Claim("sub", "123"),
+                        new Claim("scope", "foo")
+                    },
+                Version = 1
+            };
+
+            var handle = await _referenceTokenStore.StoreReferenceTokenAsync(token1);
+            var token2 = await _referenceTokenStore.GetReferenceTokenAsync(handle);
+
+            handle.Should().NotBeNull();
+            token2.Should().NotBeNull();
+            token1.ClientId.Should().Be(token2.ClientId);
+            token1.Audiences.Count.Should().Be(1);
+            token1.Audiences.First().Should().Be("aud");
+            token1.CreationTime.Should().Be(token2.CreationTime);
+            token1.Type.Should().Be(token2.Type);
+            token1.Lifetime.Should().Be(token2.Lifetime);
+            token1.Version.Should().Be(token2.Version);
+        }
+
+        [Fact(DisplayName = "GetReferenceTokenAsync should return null when token doesn't exist")]
+        public async Task FindApiResourceAsyncNull()
+        {
+            var token1 = new Token()
+            {
+                ClientId = "client",
+                Audiences = { "aud" },
+                CreationTime = DateTime.UtcNow,
+                Lifetime = 10,
+                Type = "type",
+                Claims = new List<Claim>
+                    {
+                        new Claim("sub", "123"),
+                        new Claim("scope", "foo")
+                    },
+                Version = 1
+            };
+
+            var handle = await _referenceTokenStore.StoreReferenceTokenAsync(token1);
+            var token2 = await _referenceTokenStore.GetReferenceTokenAsync("wronghandle");
+
+            handle.Should().NotBeNull();
+            token2.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "RemoveReferenceTokenAsync should remove token")]
+        public async Task RemoveReferenceTokenAsync()
+        {
+            var token1 = new Token()
+            {
+                ClientId = "client",
+                Audiences = { "aud" },
+                CreationTime = DateTime.UtcNow,
+                Lifetime = 10,
+                Type = "type",
+                Claims = new List<Claim>
+                    {
+                        new Claim("sub", "123"),
+                        new Claim("scope", "foo")
+                    },
+                Version = 1
+            };
+
+            var handle = await _referenceTokenStore.StoreReferenceTokenAsync(token1);
+            await _referenceTokenStore.RemoveReferenceTokenAsync(handle);
+
+            var token2 = await _referenceTokenStore.GetReferenceTokenAsync(handle);
+
+            handle.Should().NotBeNull();
+            token2.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "RemoveReferenceTokensAsync should remove tokens")]
+        public async Task RemoveReferenceTokensAsync()
+        {
+            var token1 = new Token()
+            {
+                ClientId = "client",
+                Audiences = { "aud" },
+                CreationTime = DateTime.UtcNow,
+                Lifetime = 10,
+                Type = "type",
+                Claims = new List<Claim>
+                    {
+                        new Claim("sub", "123"),
+                        new Claim("scope", "foo")
+                    },
+                Version = 1
+            };
+
+            var handle = await _referenceTokenStore.StoreReferenceTokenAsync(token1);
+            await _referenceTokenStore.RemoveReferenceTokensAsync("123", "client");
+            WaitForIndexing(_documentStore);
+
+            var token2 = await _referenceTokenStore.GetReferenceTokenAsync(handle);
+
+            handle.Should().NotBeNull();
+            token2.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "Parameter validation should trigger argument exceptions")]
+        public async Task Validation()
+        {
+            Assert.Throws<ArgumentException>(() => new RavenDBReferenceTokenStore(null, _documentStore));
+            Assert.Throws<ArgumentException>(() => new RavenDBReferenceTokenStore(_loggerFactory.CreateLogger<RavenDBReferenceTokenStore>(), null));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _referenceTokenStore.GetReferenceTokenAsync(null));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _referenceTokenStore.RemoveReferenceTokenAsync(null));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _referenceTokenStore.RemoveReferenceTokensAsync(null, "client"));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _referenceTokenStore.RemoveReferenceTokensAsync("123", null));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _referenceTokenStore.StoreReferenceTokenAsync(null));
+        }
+    }
+}
---
+++ src/Tests/IdentityServer4.Contrib.RavenDB.Tests/RavenDBRefreshTokenStoreTests.cs
@@ -0,0 +1,249 @@
+using Divergic.Logging.Xunit;
+using FluentAssertions;
+using IdentityServer4.Contrib.RavenDB.Stores;
+using IdentityServer4.Contrib.RavenDB.Tests.Common;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+using Microsoft.Extensions.Logging;
+using Raven.Client.Documents;
+using Raven.TestDriver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IdentityServer4.Contrib.RavenDB.Tests
+{
+    public class RavenDBRefreshTokenStoreTests : RavenIdentityServerTestBase
+    {
+        protected readonly ILoggerFactory _loggerFactory;
+        protected readonly IDocumentStore _documentStore;
+        protected readonly IRefreshTokenStore _tokenStore;
+
+        public RavenDBRefreshTokenStoreTests(ITestOutputHelper output)
+        {
+            _loggerFactory = LogFactory.Create(output);
+            _documentStore = GetDocumentStore();
+            _tokenStore = new RavenDBRefreshTokenStore(_loggerFactory.CreateLogger<RavenDBRefreshTokenStore>(), _documentStore);
+        }
+
+        [Fact(DisplayName = "Reference token should be retrievable after storage")]
+        public async Task StoreAndGetRefreshTokenAsync()
+        {
+            var token1 = new RefreshToken()
+            {
+                CreationTime = DateTime.UtcNow,
+                Lifetime = 10,
+                AccessToken = new Token
+                {
+                    ClientId = "client",
+                    Audiences = { "aud" },
+                    CreationTime = DateTime.UtcNow,
+                    Type = "type",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("sub", "123"),
+                        new Claim("scope", "foo")
+                    }
+                },
+                Version = 1
+            };
+
+            var handle = await _tokenStore.StoreRefreshTokenAsync(token1);
+            var token2 = await _tokenStore.GetRefreshTokenAsync(handle);
+
+            handle.Should().NotBeNull();
+            token2.Should().NotBeNull();
+            token2.ClientId.Should().Be(token1.ClientId);
+            token2.CreationTime.Should().Be(token1.CreationTime);
+            token2.Lifetime.Should().Be(token1.Lifetime);
+            token2.Subject.GetSubjectId().Should().Be(token1.Subject.GetSubjectId());
+            token2.Version.Should().Be(token1.Version);
+            token2.AccessToken.Audiences.Count.Should().Be(1);
+            token2.AccessToken.Audiences.First().Should().Be("aud");
+            token2.AccessToken.ClientId.Should().Be(token1.AccessToken.ClientId);
+            token2.AccessToken.CreationTime.Should().Be(token1.AccessToken.CreationTime);
+            token2.AccessToken.Type.Should().Be(token1.AccessToken.Type);
+        }
+
+        [Fact(DisplayName = "GetRefreshTokenAsync should return null when token doesn't exist")]
+        public async Task GetRefreshTokenAsyncNull()
+        {
+            var token1 = new RefreshToken()
+            {
+                CreationTime = DateTime.UtcNow,
+                Lifetime = 10,
+                AccessToken = new Token
+                {
+                    ClientId = "client",
+                    Audiences = { "aud" },
+                    CreationTime = DateTime.UtcNow,
+                    Type = "type",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("sub", "123"),
+                        new Claim("scope", "foo")
+                    }
+                },
+                Version = 1
+            };
+
+            var handle = await _tokenStore.StoreRefreshTokenAsync(token1);
+            var token2 = await _tokenStore.GetRefreshTokenAsync("wronghandle");
+
+            handle.Should().NotBeNull();
+            token2.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "RemoveRefreshTokenAsync should remove token")]
+        public async Task RemoveRefreshTokenAsync()
+        {
+            var token1 = new RefreshToken()
+            {
+                CreationTime = DateTime.UtcNow,
+                Lifetime = 10,
+                AccessToken = new Token
+                {
+                    ClientId = "client",
+                    Audiences = { "aud" },
+                    CreationTime = DateTime.UtcNow,
+                    Type = "type",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("sub", "123"),
+                        new Claim("scope", "foo")
+                    }
+                },
+                Version = 1
+            };
+
+            var handle = await _tokenStore.StoreRefreshTokenAsync(token1);
+            await _tokenStore.RemoveRefreshTokenAsync(handle);
+
+            var token2 = await _tokenStore.GetRefreshTokenAsync(handle);
+
+            handle.Should().NotBeNull();
+            token2.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "RemoveRefreshTokenAsync should on non-existing token should skip")]
+        public async Task RemoveNonExistingRefreshTokenAsync()
+        {
+            var exception = await Record.ExceptionAsync(() => _tokenStore.RemoveRefreshTokenAsync("invalidtoken"));
+            Assert.Null(exception);
+
+            exception = await Record.ExceptionAsync(() => _tokenStore.RemoveRefreshTokensAsync("invalidsub", "invalidclient"));
+            Assert.Null(exception);
+        }
+
+        [Fact(DisplayName = "RemoveRefreshTokensAsync should remove tokens")]
+        public async Task RemoveRefreshTokensAsync()
+        {
+            var token1 = new RefreshToken()
+            {
+                CreationTime = DateTime.UtcNow,
+                Lifetime = 10,
+                AccessToken = new Token
+                {
+                    ClientId = "client",
+                    Audiences = { "aud" },
+                    CreationTime = DateTime.UtcNow,
+                    Type = "type",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("sub", "123"),
+                        new Claim("scope", "foo")
+                    }
+                },
+                Version = 1
+            };
+
+            var handle = await _tokenStore.StoreRefreshTokenAsync(token1);
+            await _tokenStore.RemoveRefreshTokensAsync("123", "client");
+            WaitForIndexing(_documentStore);
+
+            var token2 = await _tokenStore.GetRefreshTokenAsync(handle);
+
+            handle.Should().NotBeNull();
+            token2.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "Reference token should be updated properly")]
+        public async Task UpdateRefreshTokenAsync()
+        {
+            var token1 = new RefreshToken()
+            {
+                CreationTime = DateTime.UtcNow,
+                Lifetime = 10,
+                AccessToken = new Token
+                {
+                    ClientId = "client",
+                    Audiences = { "aud" },
+                    CreationTime = DateTime.UtcNow,
+                    Type = "type",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("sub", "123"),
+                        new Claim("scope", "foo")
+                    }
+                },
+                Version = 1
+            };
+
+            var handle = await _tokenStore.StoreRefreshTokenAsync(token1);
+
+            var token2 = await _tokenStore.GetRefreshTokenAsync(handle);
+            token2.Lifetime = 20;
+            token2.CreationTime = DateTime.UtcNow;
+            token2.AccessToken = new Token
+            {
+                ClientId = "client2",
+                Audiences = { "aud2" },
+                CreationTime = DateTime.UtcNow,
+                Type = "type2",
+                Claims = new List<Claim>
+                    {
+                        new Claim("sub", "1234"),
+                        new Claim("scope", "foo3")
+                    }
+            };
+
+            await _tokenStore.UpdateRefreshTokenAsync(handle, token2);
+
+            var token3 = await _tokenStore.GetRefreshTokenAsync(handle);
+
+            handle.Should().NotBeNull();
+            token3.Should().NotBeNull();
+            token3.ClientId.Should().Be(token2.ClientId);
+            token3.CreationTime.Should().Be(token2.CreationTime);
+            token3.Lifetime.Should().Be(token2.Lifetime);
+            token3.Subject.GetSubjectId().Should().Be(token2.Subject.GetSubjectId());
+            token3.Version.Should().Be(token2.Version);
+            token3.AccessToken.Audiences.Count.Should().Be(1);
+            token3.AccessToken.Audiences.First().Should().Be("aud2");
+            token3.AccessToken.ClientId.Should().Be(token2.AccessToken.ClientId);
+            token3.AccessToken.CreationTime.Should().Be(token2.AccessToken.CreationTime);
+            token3.AccessToken.Type.Should().Be(token2.AccessToken.Type);
+
+            await Assert.ThrowsAsync<KeyNotFoundException>(async () => await _tokenStore.UpdateRefreshTokenAsync("invalidhandle", token2));
+        }
+
+        [Fact(DisplayName = "Parameter validation should trigger argument exceptions")]
+        public async Task Validation()
+        {
+            Assert.Throws<ArgumentException>(() => new RavenDBRefreshTokenStore(null, _documentStore));
+            Assert.Throws<ArgumentException>(() => new RavenDBRefreshTokenStore(_loggerFactory.CreateLogger<RavenDBRefreshTokenStore>(), null));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _tokenStore.GetRefreshTokenAsync(null));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _tokenStore.RemoveRefreshTokenAsync(null));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _tokenStore.RemoveRefreshTokensAsync(null, "client"));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _tokenStore.RemoveRefreshTokensAsync("123", null));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _tokenStore.StoreRefreshTokenAsync(null));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _tokenStore.UpdateRefreshTokenAsync(null, new RefreshToken()));
+            await Assert.ThrowsAsync<ArgumentException>(async () => await _tokenStore.UpdateRefreshTokenAsync("handle", null));
+        }
+    }
+}
