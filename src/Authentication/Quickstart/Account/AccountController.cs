46cf00343056e410109ad2975fff78d60ee4977f
Alexandru Puiu <alex@ops.ai>
2020-01-18 20:36:19 -0500

    Added LDAP authentication support

--- src/Authentication/Quickstart/Account/AccountController.cs
+++ src/Authentication/Quickstart/Account/AccountController.cs
@@ -3,12 +3,12 @@
 
 
 using IdentityModel;
+using IdentityServer.LdapExtension.UserStore;
 using IdentityServer4.Events;
 using IdentityServer4.Extensions;
 using IdentityServer4.Models;
 using IdentityServer4.Services;
 using IdentityServer4.Stores;
-using IdentityServer4.Test;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -28,7 +28,7 @@ namespace IdentityServer4.Quickstart.UI
     [AllowAnonymous]
     public class AccountController : Controller
     {
-        private readonly TestUserStore _users;
+        private readonly ILdapUserStore _userStore;
         private readonly IIdentityServerInteractionService _interaction;
         private readonly IClientStore _clientStore;
         private readonly IAuthenticationSchemeProvider _schemeProvider;
@@ -39,11 +39,9 @@ namespace IdentityServer4.Quickstart.UI
             IClientStore clientStore,
             IAuthenticationSchemeProvider schemeProvider,
             IEventService events,
-            TestUserStore users = null)
+            ILdapUserStore userStore)
         {
-            // if the TestUserStore is not in DI, then we'll just use the global users collection
-            // this is where you would plug in your own custom identity management library (e.g. ASP.NET Identity)
-            _users = users ?? new TestUserStore(TestUsers.Users);
+            _userStore = userStore;
 
             _interaction = interaction;
             _clientStore = clientStore;
@@ -109,9 +107,9 @@ namespace IdentityServer4.Quickstart.UI
             if (ModelState.IsValid)
             {
                 // validate username/password against in-memory store
-                if (_users.ValidateCredentials(model.Email, model.Password))
+                var user = _userStore.ValidateCredentials(model.Email, model.Password);
+                if (user != null && user.IsActive)
                 {
-                    var user = _users.FindByUsername(model.Email);
                     await _events.RaiseAsync(new UserLoginSuccessEvent(user.Username, user.SubjectId, user.Username, clientId: context?.ClientId));
 
                     // only set explicit expiration here if user chooses "remember me". 
