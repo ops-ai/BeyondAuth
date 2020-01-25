571f740b559deffd048e3ed5a4d5ba13a4b6bd24
Alexandru Puiu <alex@ops.ai>
2020-01-24 23:36:17 -0500

    Added logo and updated footer on SPA

--- src/BeyondAuth.Web/ClientApp/src/app/@theme/components/footer/footer.component.ts
+++ src/BeyondAuth.Web/ClientApp/src/app/@theme/components/footer/footer.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2019</span>
+    <span class="created-by">Created with ♥ by <b><a href="https://ops.ai" target="_blank">opsAI LLC</a></b> 2020.</span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
+      <a href="https://github.com/ops-ai/beyondauth" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
       <a href="#" target="_blank" class="ion ion-social-twitter"></a>
       <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
