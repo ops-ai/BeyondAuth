1e5e8e6d3816ce2d7f734bd47f577047150d93a1
Alexandru Puiu <alex@ops.ai>
2020-01-25 23:01:03 -0500

    Added ACL authorization handler project with basic requirements

---
+++ src/Authorization Handlers/BeyondAuth.Acl/ISecurableEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BeyondAuth.Acl
+{
+    public interface ISecurableEntity
+    {
+        /// <summary>
+        /// Resource unique identifier
+        /// </summary>
+        string Id { get; }
+
+        /// <summary>
+        /// Resource parent
+        /// </summary>
+        string ParentId { get; }
+
+        /// <summary>
+        /// Resource Owner
+        /// </summary>
+        string OwnerId { get; }
+    }
+}
