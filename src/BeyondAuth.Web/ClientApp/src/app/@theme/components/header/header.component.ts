c96c15a510b5327571133c1e462f5decf1e0dea5
Alexandru Puiu <alex@ops.ai>
2020-01-25 22:49:42 -0500

    Added links and icons to user menu

--- src/BeyondAuth.Web/ClientApp/src/app/@theme/components/header/header.component.ts
+++ src/BeyondAuth.Web/ClientApp/src/app/@theme/components/header/header.component.ts
@@ -34,7 +34,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   currentTheme = 'default';
 
-  userMenu = [ { title: 'Profile' }, { title: 'Log out' } ];
+  userMenu = [{ title: 'My Profile', link: './profile', icon:'person' }, { title: 'Billing History', link: './billing', icon: 'file-text' },  { title: 'Log out', link: '../auth/login', icon: 'lock' }];
 
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
