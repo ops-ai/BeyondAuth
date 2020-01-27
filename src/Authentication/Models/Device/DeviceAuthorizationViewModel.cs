36f3ce6e4ed2fccebb1a9f61f5169e7a92261cde
Alexandru Puiu <alex@ops.ai>
2020-01-26 21:36:06 -0500

    Reorganized controllers and models into separate folders

---
+++ src/Authentication/Models/Device/DeviceAuthorizationViewModel.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Authentication.Models.Consent;
+
+namespace Authentication.Models.Device
+{
+    public class DeviceAuthorizationViewModel : ConsentViewModel
+    {
+        public string UserCode { get; set; }
+
+        public bool ConfirmUserCode { get; set; }
+    }
+}
