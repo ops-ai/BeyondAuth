84cd5e4935a95e6822f22d807352bf4cda8c58b1
Alexandru Puiu <alex@ops.ai>
2020-01-26 21:37:20 -0500

    Added more useful info to error page

--- src/Authentication/Models/ErrorViewModel.cs
+++ src/Authentication/Models/ErrorViewModel.cs
@@ -1,9 +1,28 @@
+using IdentityServer4.Models;
+
 namespace Authentication.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public ErrorViewModel()
+        {
+        }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public ErrorViewModel(string error) => Error = new ErrorMessage { Error = error };
+
+        /// <summary>
+        /// IdentityServer error
+        /// </summary>
+        public ErrorMessage Error { get; set; }
+
+        /// <summary>
+        /// Support email to display
+        /// </summary>
+        public string SupportEmail { get; set; } = "support@beyondauth.io";
+
+        /// <summary>
+        /// Current app version and instance id
+        /// </summary>
+        public string Version { get; set; }
     }
 }
