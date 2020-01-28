7ef5ff9637e31d642b13a5c07ab97c7ea0df2c8a
Alexandru Puiu <alex@ops.ai>
2020-01-27 23:49:05 -0500

    Moved account and consent options to be loaded from config using Options pattern

---
+++ src/Authentication/Options/AccountOptions.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+
+namespace Authentication.Models.Account
+{
+    public class AccountOptions
+    {
+        /// <summary>
+        /// Allow email + password login
+        /// </summary>
+        public bool AllowLocalLogin { get; set; }
+        
+        /// <summary>
+        /// Allow remembering login
+        /// </summary>
+        public bool AllowRememberLogin { get; set; }
+
+        /// <summary>
+        /// Remember me cookie lifetime
+        /// </summary>
+        public TimeSpan RememberMeLoginDuration { get; set; }
+
+        /// <summary>
+        /// Show logout prompt
+        /// </summary>
+        public bool ShowLogoutPrompt { get; set; }
+
+        /// <summary>
+        /// Redirect automatically after signout
+        /// </summary>
+        public bool AutomaticRedirectAfterSignOut { get; set; }
+
+        /// <summary>
+        /// The windows authentication scheme used
+        /// </summary>
+        public string WindowsAuthenticationSchemeName { get; set; }
+
+        /// <summary>
+        /// if user uses windows auth, should we load the groups from windows
+        /// </summary>
+        public bool IncludeWindowsGroups { get; set; }
+
+        /// <summary>
+        /// Invalid credentials error message
+        /// </summary>
+        public string InvalidCredentialsErrorMessage { get; set; }
+    }
+}
