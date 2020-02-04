635a8efea6507aa2d195d360d0ad0162adecf795
Alexandru Puiu <alex@ops.ai>
2020-02-03 23:49:45 -0500

    Added test angular footer spec

---
+++ src/BeyondAuth.Web/ClientApp/src/app/@theme/components/footer/footer.component.spec.ts
@@ -0,0 +1,9 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { FooterComponent } from './footer.component';
+
+describe('FooterComponent', () => {
+  it('raises the selected event when clicked', () => {
+    expect("testing")
+      .toBe("testing", 'service returned stub value');
+  });
+});
