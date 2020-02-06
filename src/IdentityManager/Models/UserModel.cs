21b25295f2f439b76a78984fc6ab095a63f74f02
Alexandru Puiu <alex@ops.ai>
2020-02-05 22:44:24 -0500

    Added Identity Manager controller stubs for managing users, oidc clients and api resources

---
+++ src/IdentityManager/Models/UserModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// User ID
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// First Name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last Name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Display Name
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Email Address
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Organization
+        /// </summary>
+        public string Organization { get; set; }
+
+        /// <summary>
+        /// Phone Numbers
+        /// </summary>
+        public List<string> PhoneNumbers { get; set; }
+
+        /// <summary>
+        /// Allow user to reset his own password
+        /// </summary>
+        public bool PasswordResetAllowed { get; set; }
+
+        /// <summary>
+        /// Allow user to change his own password
+        /// </summary>
+        public bool ChangePasswordAllowed { get; set; }
+
+        /// <summary>
+        /// Password policy
+        /// </summary>
+        public string PasswordPolicy { get; set; }
+
+        /// <summary>
+        /// Account is disabled
+        /// </summary>
+        public bool Disabled { get; set; }
+
+        /// <summary>
+        /// Account is locked
+        /// </summary>
+        public bool Locked { get; set; }
+
+        /// <summary>
+        /// The time when the lockout is set to expire
+        /// </summary>
+        public DateTime? LockoutEnd { get; set; }
+
+        /// <summary>
+        /// Date last logged in successfully
+        /// </summary>
+        public DateTime? LastLoggedIn { get; set; }
+
+        /// <summary>
+        /// Date the user's password expires
+        /// </summary>
+        public DateTime? PasswordExpiry { get; set; }
+
+        /// <summary>
+        /// Date the user's account expires
+        /// </summary>
+        public DateTime? AccountExpiration { get; set; }
+
+        /// <summary>
+        /// Claims
+        /// </summary>
+        public Dictionary<string, string> Claims { get; set; } = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Require the user to change their password the next time they log in
+        /// </summary>
+        public bool ChangePasswordOnNextLogin { get; set; } = false;
+
+        /// <summary>
+        /// Optional one-time access token to auto-login
+        /// </summary>
+        public string Otac { get; set; }
+
+        /// <summary>
+        /// TimeZone info for current user in IANA format. (e.g. America/Los_Angeles)
+        /// </summary>
+        public string ZoneInfo { get; set; }
+    }
+}
---
+++ src/IdentityManager/Models/UserUpdateModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UserUpdateModel
+    {
+        /// <summary>
+        /// First Name
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last Name
+        /// </summary>
+        [Required]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Display Name
+        /// </summary>
+        [Required]
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Email Address
+        /// </summary>
+        [EmailAddress(ErrorMessage = "Not a valid email")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Organization
+        /// </summary>
+        [Required]
+        public string Organization { get; set; }
+
+        /// <summary>
+        /// Allow user to reset his own password
+        /// </summary>
+        public bool PasswordResetAllowed { get; set; } = true;
+
+        /// <summary>
+        /// Allow user to reset his own password
+        /// </summary>
+        public bool ChangePasswordAllowed { get; set; } = true;
+
+        /// <summary>
+        /// Require the user to change their password the next time they log in
+        /// </summary>
+        public bool? ChangePasswordOnNextLogin { get; set; } = false;
+
+        /// <summary>
+        /// Password policy
+        /// </summary>
+        public string PasswordPolicy { get; set; } = null;
+
+        /// <summary>
+        /// Account expiration date
+        /// </summary>
+        public DateTime? AccountExpiration { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Account is disabled
+        /// </summary>
+        public bool Disabled { get; set; } = false;
+
+        /// <summary>
+        /// Account is locked
+        /// </summary>
+        public bool? Locked { get; set; }
+
+        /// <summary>
+        /// Claims
+        /// </summary>
+        public Dictionary<string, string> Claims { get; set; } = new Dictionary<string, string>();
+
+        /// <summary>
+        /// TimeZone info for current user in IANA format. (e.g. America/Los_Angeles)
+        /// </summary>
+        public string ZoneInfo { get; set; }
+    }
+}
