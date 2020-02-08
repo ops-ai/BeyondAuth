bf43c5923749343d4ec4690798d3dba7a6608c3f
Alexandru Puiu <alex@ops.ai>
2020-02-08 12:57:43 -0500

    Added missing properties to ACL ISecurableEntity

--- src/Authorization Handlers/BeyondAuth.Acl/ISecurableEntity.cs
+++ src/Authorization Handlers/BeyondAuth.Acl/ISecurableEntity.cs
@@ -1,4 +1,6 @@
-﻿namespace BeyondAuth.Acl
+﻿using System.Collections.Generic;
+
+namespace BeyondAuth.Acl
 {
     public interface ISecurableEntity
     {
@@ -16,5 +18,20 @@
         /// Resource Owner
         /// </summary>
         string OwnerId { get; }
+
+        /// <summary>
+        /// Id of the nearest parent that contains the ACEs
+        /// </summary>
+        public string NearestSecurityHolderId { get; set; }
+
+        /// <summary>
+        /// Nesting level of inheritance
+        /// </summary>
+        public int Level { get; set; }
+
+        /// <summary>
+        /// List of ACEs storing permissions for the secured entity
+        /// </summary>
+        public List<AceEntry> AceEntries { get; set; }
     }
 }
