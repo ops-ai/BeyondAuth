befb1f6d0b9ca8ee888a3f00296f883bd6095259
Alexandru Puiu <alex@ops.ai>
2020-02-08 13:00:36 -0500

    Code cleanup

--- src/IdentityManager/Program.cs
+++ src/IdentityManager/Program.cs
@@ -1,16 +1,13 @@
-using System.IO;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace IdentityManager
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
