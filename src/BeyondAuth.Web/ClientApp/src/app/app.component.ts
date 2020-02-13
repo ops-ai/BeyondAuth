d7c066f301796772bb0d9b8065ec08263a8e1547
Alexandru Puiu <alex@ops.ai>
2020-02-13 01:38:01 -0500

    Added oidc auth to angular app

--- src/BeyondAuth.Web/ClientApp/src/app/app.component.ts
+++ src/BeyondAuth.Web/ClientApp/src/app/app.component.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'ngx-app',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'BeyondAuth';
+
+  isAuthenticated: boolean;
+  userData: any;
+
+  constructor(public oidcSecurityService: OidcSecurityService) {
+    if (this.oidcSecurityService.moduleSetup) {
+      this.doCallbackLogicIfRequired();
+    } else {
+      this.oidcSecurityService.onModuleSetup.subscribe(() => {
+        this.doCallbackLogicIfRequired();
+      });
+    }
+  }
+
+  ngOnInit() {
+    this.oidcSecurityService.getIsAuthorized().subscribe(auth => {
+      this.isAuthenticated = auth;
+    });
+
+    this.oidcSecurityService.getUserData().subscribe(userData => {
+      this.userData = userData;
+    });
+  }
+
+  ngOnDestroy(): void { }
+
+  login() {
+    this.oidcSecurityService.authorize();
+  }
+
+  logout() {
+    this.oidcSecurityService.logoff();
+  }
+
+  private doCallbackLogicIfRequired() {
+    // Will do a callback, if the url has a code and state parameter.
+    this.oidcSecurityService.authorizedCallbackWithCode(window.location.toString());
+  }
 }
