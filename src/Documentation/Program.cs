e6c2dad7dd66de893b43d7019d4ef4f2c6eb478f
Alexandru Puiu <alex@ops.ai>
2020-02-14 23:31:12 -0500

    Added documentation project

---
+++ src/Documentation/Program.cs
@@ -0,0 +1,21 @@
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+namespace Documentation
+{
+    public class Program
+    {
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/Documentation/Startup.cs
@@ -0,0 +1,119 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CorrelationId.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Autofac;
+using NSwag.AspNetCore;
+
+namespace Documentation
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public ILifetimeScope AutofacContainer { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardLimit = 1;
+                Configuration["ProxyNodes"]?.Split(';').ToList().ForEach(t =>
+                {
+                    if (!string.IsNullOrEmpty(t))
+                        options.KnownProxies.Add(IPAddress.Parse(t));
+                });
+            });
+
+            services.AddCorrelationId();
+
+            services.AddHealthChecks();
+
+            services.AddOpenApiDocument(config =>
+            {
+                config.DocumentName = "v1";
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "BeyondAuth";
+                    document.Info.Description = File.ReadAllText("readme.md");
+                    document.Info.ExtensionData = new Dictionary<string, object>
+                    {
+                        { "x-logo", new { url = "/logo.png", altText = "BeyondAuth" } }
+                    };
+
+                    document.Info.Contact = new OpenApiContact
+                    {
+                        Name = "opsAI Support",
+                        Email = "support@ops.ai",
+                        Url = "https://support.ops.ai"
+                    };
+
+                    document.Servers.Add(new OpenApiServer { Url = "https://api.apis.guru/v2/specs/instagram.com/1.0.0/swagger.yaml", Description = "test" });
+                };
+
+                config.AddSecurity("bearer", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.OpenIdConnect,
+                    Flow = OpenApiOAuth2Flow.AccessCode,
+                    OpenIdConnectUrl = $"{Configuration["AuthorityUrl"]}.well-known/openid-configuration",
+                    Scopes = new Dictionary<string, string>
+                    {
+
+                    }
+                });
+                config.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("bearer"));
+
+                config.SerializerSettings = new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                };
+                config.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseOpenApi();
+            app.UseReDoc(settings =>
+            {
+                settings.Path = "/redoc";
+            });
+
+            app.UseSwaggerUi3();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
