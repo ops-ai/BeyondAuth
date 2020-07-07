cb4b57ee6f98c9dda138d2fc967b595ba45ffb97
Alexandru Puiu <alex@ops.ai>
2020-07-07 08:50:18 -0400

    Added multi-tenancy support to authentication project

---
+++ src/Authentication/Options/BrandingOptions.cs
@@ -0,0 +1,38 @@
+﻿namespace Authentication.Models.Account
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BrandingOptions
+    {
+        /// <summary>
+        /// Logo
+        /// </summary>
+        public string Logo { get; set; }
+
+        /// <summary>
+        /// Theme
+        /// </summary>
+        public string Theme { get; set; }
+
+        /// <summary>
+        /// Primary Color
+        /// </summary>
+        public string PrimaryColor { get; set; }
+
+        /// <summary>
+        /// Secondary Color
+        /// </summary>
+        public string SecondaryColor { get; set; }
+
+        /// <summary>
+        /// Optional link for help page on login page
+        /// </summary>
+        public string SupportLink { get; set; }
+
+        /// <summary>
+        /// Display text for support link
+        /// </summary>
+        public string SupportMessage { get; set; }
+    }
+}
