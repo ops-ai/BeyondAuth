677325f3d52de300c6557f6069a8d3fdf251460e
Alexandru Puiu <alex@ops.ai>
2021-04-28 01:19:59 -0400

    Added hangfire authentication

---
+++ src/BeyondAuth.Web/Services/HangfireAuthorizationFilter.cs
@@ -0,0 +1,23 @@
+﻿using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Http;
+
+namespace BeyondAuth.Web.Services
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HangfireAuthorizationFilter(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public bool Authorize(DashboardContext context)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            
+            // Allow all authenticated users to see the Dashboard (potentially dangerous).
+            return httpContext.User.Identity.IsAuthenticated && httpContext.User.IsInRole("Admin");
+        }
+    }
+}
