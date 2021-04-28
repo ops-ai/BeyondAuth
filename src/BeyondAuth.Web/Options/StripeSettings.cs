ec5b8c9f45dfa7ed80a7f3e3c4018dfa5cff0868
Alexandru Puiu <alex@ops.ai>
2021-04-28 01:20:53 -0400

    Added setting holder for Stripe

---
+++ src/BeyondAuth.Web/Options/StripeSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BeyondAuth.Web.Options
+{
+    public class StripeSettings
+    {
+        public string ApiKey { get; set; }
+
+        public string StripeAccount { get; set; }
+    }
+}
