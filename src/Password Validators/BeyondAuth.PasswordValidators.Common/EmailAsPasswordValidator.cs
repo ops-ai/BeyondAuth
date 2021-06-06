3a596d4ae158eb2d163f2bdd47e4d370e07d36dd
Alexandru Puiu <alex@ops.ai>
2021-06-06 00:45:40 -0400

    Added new project for common password validators including validators for password same as username and password included in email address

---
+++ src/Password Validators/BeyondAuth.PasswordValidators.Common/EmailAsPasswordValidator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Raven.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace BeyondAuth.PasswordValidators.Common
+{
+    public class EmailAsPasswordValidator<TUser> : IPasswordValidator<TUser> where TUser : IdentityUser
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<TUser> manager, TUser user, string password)
+        {
+            if (string.Equals(user.Email, password, StringComparison.OrdinalIgnoreCase))
+            {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError
+                {
+                    Code = "EmailAsPassword",
+                    Description = "Password cannot be the same as the email address"
+                }));
+            }
+            if (user.Email?.Contains(password, StringComparison.OrdinalIgnoreCase) ?? false)
+            {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError
+                {
+                    Code = "EmailContainsPassword",
+                    Description = "Password cannot be part of the email address"
+                }));
+            }
+            return Task.FromResult(IdentityResult.Success);
+        }
+    }
+}
---
+++ src/Password Validators/BeyondAuth.PasswordValidators.Common/InvalidPhrasePasswordValidator.cs
@@ -0,0 +1,33 @@
+﻿// License: MIT
+// Source (with minor modifications): https://github.com/andrewlock/NetEscapades.AspNetCore.Identity.Validators
+
+using Microsoft.AspNetCore.Identity;
+using Raven.Identity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BeyondAuth.PasswordValidators.Common
+{
+    public class InvalidPhrasePasswordValidator<TUser> : IPasswordValidator<TUser> where TUser : IdentityUser
+    {
+        private readonly HashSet<string> _invalidPhrases;
+        public InvalidPhrasePasswordValidator(IEnumerable<string> invalidPhrases)
+        {
+            if (invalidPhrases == null) { throw new ArgumentNullException(nameof(invalidPhrases)); }
+            _invalidPhrases = new HashSet<string>(invalidPhrases, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public Task<IdentityResult> ValidateAsync(UserManager<TUser> manager, TUser user, string password)
+        {
+            if (password == null) { throw new ArgumentNullException(nameof(password)); }
+            if (manager == null) { throw new ArgumentNullException(nameof(manager)); }
+
+            var result = _invalidPhrases.Contains(password) ? 
+                IdentityResult.Failed(new IdentityError { Code = "InvalidPhrase", Description = $"You cannot use '{password}' as your password" }) : 
+                IdentityResult.Success;
+
+            return Task.FromResult(result);
+        }
+    }
+}
