f5e13f017b5f3871207db59a7049b8f28dd6913e
Alexandru Puiu <alex@ops.ai>
2021-07-16 17:43:38 -0400

    Added Identity Server event sinks to publish as EventSource Added Stats event sinks for recording IdentityServer events to ravendb

---
+++ src/Authentication/Infrastructure/IdentityServerEventSource.cs
@@ -0,0 +1,154 @@
+﻿using IdentityServer4.Events;
+using System.Diagnostics.Tracing;
+
+namespace Authentication.Infrastructure
+{
+    /// <summary>
+    /// Event source implementation for IdentityServer4 event sinks
+    /// </summary>
+    [EventSource(Name = "IdentityServer")]
+    public class IdentityServerEventSource : EventSource
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IdentityServerEventSource Log = new();
+
+        /// <summary>
+        /// Event keywords
+        /// </summary>
+        private class Keywords
+        {
+            public const EventKeywords Api = (EventKeywords)1;
+            public const EventKeywords Client = (EventKeywords)2;
+            public const EventKeywords Token = (EventKeywords)4;
+            public const EventKeywords Perf = (EventKeywords)8;
+            public const EventKeywords TokenIntrospection = (EventKeywords)16;
+            public const EventKeywords TokenRevoked = (EventKeywords)32;
+            public const EventKeywords UserLogin = (EventKeywords)64;
+            public const EventKeywords UserLogout = (EventKeywords)128;
+            public const EventKeywords Consent = (EventKeywords)256;
+            public const EventKeywords UnhandledException = (EventKeywords)512;
+            public const EventKeywords DeviceAuthorization = (EventKeywords)1024;
+        }
+
+        /// <summary>
+        /// Api Authentication Success
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(1, Message = "Api Authentication Success: {0}", Level = EventLevel.Informational, Keywords = Keywords.Api)] 
+        public void ApiAuthenticationSuccess(ApiAuthenticationSuccessEvent evt) { WriteEvent(1, evt.ApiName, evt); }
+
+        /// <summary>
+        /// Api Authentication Failure
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(2, Message = "Api Authentication Failure: {0}", Level = EventLevel.Warning, Keywords = Keywords.Api)]
+        public void ApiAuthenticationFailure(ApiAuthenticationFailureEvent evt) { WriteEvent(2, evt.ApiName, evt); }
+
+        /// <summary>
+        /// Client Authentication Success
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(3, Message = "Client Authentication Success: {0}", Level = EventLevel.Informational, Keywords = Keywords.Client)]
+        public void ClientAuthenticationSuccess(ClientAuthenticationSuccessEvent evt) { WriteEvent(3, evt.ClientId, evt); }
+
+        /// <summary>
+        /// Client Authentication Failure
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(4, Message = "Client Authentication Failure: {0}", Level = EventLevel.Warning, Keywords = Keywords.Client)]
+        public void ClientAuthenticationFailure(ClientAuthenticationFailureEvent evt) { WriteEvent(4, evt.ClientId, evt); }
+
+        /// <summary>
+        /// Token Issued Success
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(5, Message = "Token Issued Success: {0}", Level = EventLevel.Informational, Keywords = Keywords.Token)]
+        public void TokenIssuedSuccess(TokenIssuedSuccessEvent evt) { WriteEvent(5, evt.GrantType, evt); }
+
+        /// <summary>
+        /// Token Issued Failure
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(6, Message = "Token Issued Failure: {0}", Level = EventLevel.Warning, Keywords = Keywords.Token)]
+        public void TokenIssuedFailure(TokenIssuedFailureEvent evt) { WriteEvent(6, evt.GrantType, evt); }
+
+        /// <summary>
+        /// Token Introspection Success
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(7, Message = "Token Introspection Success: {0}", Level = EventLevel.Informational, Keywords = Keywords.TokenIntrospection)]
+        public void TokenIntrospectionSuccess(TokenIntrospectionSuccessEvent evt) { WriteEvent(7, evt.Token, evt); }
+
+        /// <summary>
+        /// Token Introspection Failure
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(8, Message = "Token Introspection Failure: {0}", Level = EventLevel.Warning, Keywords = Keywords.TokenIntrospection)]
+        public void TokenIntrospectionFailure(TokenIntrospectionFailureEvent evt) { WriteEvent(8, evt.Token, evt); }
+
+        /// <summary>
+        /// Token Revoked Success
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(9, Message = "Token Revoked Success: {0}", Level = EventLevel.Informational, Keywords = Keywords.TokenRevoked)]
+        public void TokenRevokedSuccess(TokenRevokedSuccessEvent evt) { WriteEvent(9, evt.ClientId, evt); }
+
+        /// <summary>
+        /// User Login Success
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(10, Message = "User Login Success: {0}", Level = EventLevel.Informational, Keywords = Keywords.UserLogin)]
+        public void UserLoginSuccess(UserLoginSuccessEvent evt) { WriteEvent(10, evt.ClientId, evt); }
+
+        /// <summary>
+        /// User Login Failure
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(11, Message = "User Login Failure: {0}", Level = EventLevel.Warning, Keywords = Keywords.UserLogin)]
+        public void UserLoginFailure(UserLoginFailureEvent evt) { WriteEvent(11, evt.ClientId, evt); }
+
+        /// <summary>
+        /// User Logout Success
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(12, Message = "User Logout Success: {0}", Level = EventLevel.Informational, Keywords = Keywords.UserLogout)]
+        public void UserLogoutSuccess(UserLogoutSuccessEvent evt) { WriteEvent(12, evt.SubjectId, evt); }
+
+        /// <summary>
+        /// Consent Granted
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(13, Message = "Consent Granted: {0}", Level = EventLevel.Informational, Keywords = Keywords.Consent)]
+        public void ConsentGranted(ConsentGrantedEvent evt) { WriteEvent(13, evt.ClientId, evt); }
+
+        /// <summary>
+        /// Consent Denied
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(14, Message = "Consent Denied: {0}", Level = EventLevel.Informational, Keywords = Keywords.Consent)]
+        public void ConsentDenied(ConsentDeniedEvent evt) { WriteEvent(14, evt.ClientId, evt); }
+
+        /// <summary>
+        /// Unhandled Exception
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(15, Message = "Unhandled Exception: {0}", Level = EventLevel.Error, Keywords = Keywords.UnhandledException)]
+        public void UnhandledException(UnhandledExceptionEvent evt) { WriteEvent(15, evt.Category, evt); }
+
+        /// <summary>
+        /// Device Authorization Success
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(16, Message = "Device Authorization Success: {0}", Level = EventLevel.Informational, Keywords = Keywords.DeviceAuthorization)]
+        public void DeviceAuthorizationSuccess(DeviceAuthorizationSuccessEvent evt) { WriteEvent(16, evt.ClientId, evt); }
+
+        /// <summary>
+        /// Device Authorization Failure
+        /// </summary>
+        /// <param name="evt"></param>
+        [Event(17, Message = "Device Authorization Failure: {0}", Level = EventLevel.Warning, Keywords = Keywords.DeviceAuthorization)]
+        public void DeviceAuthorizationFailure(DeviceAuthorizationFailureEvent evt) { WriteEvent(17, evt.ClientId, evt); }
+    }
+}
---
+++ src/Authentication/Infrastructure/IdentityServerStatsSink.cs
@@ -0,0 +1,105 @@
+﻿using IdentityServer4.Contrib.RavenDB.Options;
+using IdentityServer4.Events;
+using IdentityServer4.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Raven.Client.Documents;
+using System;
+using System.Threading.Tasks;
+
+namespace Authentication.Infrastructure
+{
+    /// <summary>
+    /// IdentityServer4 event sinks to RavenDb time series stats recorder
+    /// </summary>
+    public class IdentityServerStatsSink : IEventSink
+    {
+        private readonly ILogger _logger;
+        private readonly IDocumentStore _store;
+        private readonly IOptions<IdentityStoreOptions> _identityStoreOptions;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="store"></param>
+        /// <param name="identityStoreOptions"></param>
+        public IdentityServerStatsSink(ILogger<IdentityServerStatsSink> logger, IDocumentStore store, IOptions<IdentityStoreOptions> identityStoreOptions)
+        {
+            _logger = logger ?? throw new ArgumentException("loggerFactory is required", nameof(logger));
+            _store = store ?? throw new ArgumentException("store is required", nameof(store));
+            _identityStoreOptions = identityStoreOptions;
+        }
+
+        /// <summary>
+        /// Persist events
+        /// </summary>
+        /// <param name="evt"></param>
+        /// <returns></returns>
+        public async Task PersistAsync(Event evt)
+        {
+            using (var session = _store.OpenAsyncSession(_identityStoreOptions?.Value.DatabaseName))
+            {
+                switch (evt)
+                {
+                    case ApiAuthenticationSuccessEvent authenticationSuccessEvent:
+                        session.TimeSeriesFor($"ApiResources/{authenticationSuccessEvent.ApiName}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                    case ApiAuthenticationFailureEvent apiAuthenticationFailureEvent:
+                        session.TimeSeriesFor($"ApiResources/{apiAuthenticationFailureEvent.ApiName}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+
+                    case ClientAuthenticationSuccessEvent clientAuthSuccess:
+                        session.TimeSeriesFor($"Clients/{clientAuthSuccess.ClientId}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                    case ClientAuthenticationFailureEvent clientAuthFailure:
+                        session.TimeSeriesFor($"Clients/{clientAuthFailure.ClientId}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                    case TokenIssuedSuccessEvent tokenIssuedSuccess:
+                        session.TimeSeriesFor($"Clients/{tokenIssuedSuccess.ClientId}", evt.Name).Append(evt.TimeStamp, 1, tokenIssuedSuccess.SubjectId);
+                        session.TimeSeriesFor(tokenIssuedSuccess.SubjectId, evt.Name).Append(evt.TimeStamp, 1, $"Clients/{tokenIssuedSuccess.ClientId}");
+                        break;
+                    case TokenIssuedFailureEvent tokenIssuedFailure:
+                        session.TimeSeriesFor($"Clients/{tokenIssuedFailure.ClientId}", evt.Name).Append(evt.TimeStamp, 1, tokenIssuedFailure.SubjectId);
+                        session.TimeSeriesFor(tokenIssuedFailure.SubjectId, evt.Name).Append(evt.TimeStamp, 1, $"Clients/{tokenIssuedFailure.ClientId}");
+                        break;
+                    case TokenIntrospectionSuccessEvent tokenIntrospectionSuccess:
+                        session.TimeSeriesFor($"ApiResources/{tokenIntrospectionSuccess.ApiName}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                    case TokenIntrospectionFailureEvent tokenIntrospectionFailure:
+                        session.TimeSeriesFor($"ApiResources/{tokenIntrospectionFailure.ApiName}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                    case TokenRevokedSuccessEvent tokenRevokedSuccess:
+                        session.TimeSeriesFor($"Clients/{tokenRevokedSuccess.ClientId}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                    case UserLoginSuccessEvent userLoginSuccess:
+                        session.TimeSeriesFor($"Clients/{userLoginSuccess.ClientId}", evt.Name).Append(evt.TimeStamp, 1, userLoginSuccess.SubjectId);
+                        session.TimeSeriesFor(userLoginSuccess.SubjectId, evt.Name).Append(evt.TimeStamp, 1, $"Clients/{userLoginSuccess.ClientId}");
+                        break;
+                    case UserLoginFailureEvent userLoginFailure:
+                        session.TimeSeriesFor($"Clients/{userLoginFailure.ClientId}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                    case UserLogoutSuccessEvent userLogoutSuccess:
+                        session.TimeSeriesFor(userLogoutSuccess.SubjectId, evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                    case ConsentGrantedEvent consentGranted:
+                        session.TimeSeriesFor($"Clients/{consentGranted.ClientId}", evt.Name).Append(evt.TimeStamp, 1, consentGranted.SubjectId);
+                        session.TimeSeriesFor(consentGranted.SubjectId, evt.Name).Append(evt.TimeStamp, 1, $"Clients/{consentGranted.ClientId}");
+                        break;
+                    case ConsentDeniedEvent consentDenied:
+                        session.TimeSeriesFor($"Clients/{consentDenied.ClientId}", evt.Name).Append(evt.TimeStamp, 1, consentDenied.SubjectId);
+                        session.TimeSeriesFor(consentDenied.SubjectId, evt.Name).Append(evt.TimeStamp, 1, $"Clients/{consentDenied.ClientId}");
+                        break;
+
+                    case DeviceAuthorizationSuccessEvent deviceAuthorizationSuccess:
+                        session.TimeSeriesFor($"Clients/{deviceAuthorizationSuccess.ClientId}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                    case DeviceAuthorizationFailureEvent deviceAuthorizationFailure:
+                        session.TimeSeriesFor($"Clients/{deviceAuthorizationFailure.ClientId}", evt.Name).Append(evt.TimeStamp, 1);
+                        break;
+                }
+                await session.SaveChangesAsync();
+            }
+        }
+    }
+}
