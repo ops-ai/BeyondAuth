45c2d092141efcfed6731b68942763149c78c117
Alexandru Puiu <alex@ops.ai>
2021-12-27 21:26:10 -0500

    Added feature http context provider

---
+++ src/Authentication/Options/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Authentication.Options
+{
+    public static class Constants
+    {
+        public static class FeatureFlags
+        {
+            public static string PasswordReset = "PasswordReset";
+        }
+    }
+}
