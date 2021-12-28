010787878b61fca95f40071178942f7150bc7b9a
Alexandru Puiu <alex@ops.ai>
2021-12-27 21:26:10 -0500

    Added feature http context provider

---
+++ src/Authentication/Infrastructure/HttpContextTargetingContextAccessor.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.FeatureManagement.FeatureFilters;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Authentication.Infrastructure
+{
+    public class HttpContextTargetingContextAccessor : ITargetingContextAccessor
+    {
+        private const string TargetingContextLookup = "HttpContextTargetingContextAccessor.TargetingContext";
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HttpContextTargetingContextAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public ValueTask<TargetingContext> GetContextAsync()
+        {
+            HttpContext httpContext = _httpContextAccessor.HttpContext;
+
+            //
+            // Try cache lookup
+            if (httpContext.Items.TryGetValue(TargetingContextLookup, out object value))
+            {
+                return new ValueTask<TargetingContext>((TargetingContext)value);
+            }
+
+            ClaimsPrincipal user = httpContext.User;
+
+            List<string> groups = new List<string>();
+
+            //
+            // This application expects groups to be specified in the user's claims
+            foreach (Claim claim in user.Claims)
+            {
+                if (claim.Type == "group")
+                {
+                    groups.Add(claim.Value);
+                }
+            }
+
+            //
+            // Build targeting context based off user info
+            TargetingContext targetingContext = new TargetingContext
+            {
+                UserId = user.Identity.Name,
+                Groups = groups
+            };
+
+            //
+            // Cache for subsequent lookup
+            httpContext.Items[TargetingContextLookup] = targetingContext;
+
+            return new ValueTask<TargetingContext>(targetingContext);
+        }
+    }
+}
