b914389195de137d91faa3571a590b7137f710a2
Alexandru Puiu <alex@ops.ai>
2022-02-12 20:13:28 -0500

    Implemented subscribing to all tenants databases in authentication service to send password reset emails

--- src/Authentication/Services/PasswordResetService.cs
+++ src/Authentication/Services/PasswordResetService.cs
@@ -4,36 +4,91 @@ using Raven.Client.Documents;
 using System.Threading;
 using System.Threading.Tasks;
 using System;
+using Authentication.Models.Messages;
+using System.Net;
+using Authentication.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 
 namespace Authentication.Services
 {
     public class PasswordResetService : BackgroundService
     {
         private readonly IDocumentStore _store;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEmailSender _emailSender;
+        private readonly IEmailService _emailService;
+        private readonly ILogger _logger;
 
-        public PasswordResetService(IDocumentStore store)
+        public PasswordResetService(IDocumentStore store, IEmailSender emailSender, IEmailService emailService, ILoggerFactory loggerFactory)
         {
             _store = store;
-
+            _emailSender = emailSender;
+            _emailService = emailService;
+            _logger = loggerFactory.CreateLogger<PasswordResetService>();
         }
 
-        private Task SendPassword(string userId)
+        private async Task SendPasswordAsync(string requestId, string tenant)
         {
+            _logger.LogInformation("Processing password reset request {requestId} in {tenant}", requestId, tenant);
+            try
+            {
+                using (var session = _store.OpenAsyncSession($"TenantIdentity-{tenant}"))
+                {
+                    var request = await session.Include<PasswordResetRequest>(t => t.UserId).LoadAsync<PasswordResetRequest>(requestId);
+                    if (request == null || request.Handled)
+                        return;
 
+                    var user = await session.LoadAsync<ApplicationUser>(request.UserId);
+                    if (user == null)
+                        return;
 
-            return Task.FromResult(0);
+                    var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+                    // Url.Action auto encodes params you pass to it, but ReturnUrl is already encoded. Don't double encode
+                    var callbackUrl = $"https://{tenant}/reset-password?code={code}";
+
+                    var emailMessage = new ResetPasswordEmailMessage { To = user.Email, FirstName = user.FirstName, CallbackUrl = callbackUrl };
+
+                    var bodyHtml = await _emailService.RenderPartialViewToString("ResetPassword.html", emailMessage, null);
+                    var bodyTxt = await _emailService.RenderPartialViewToString("ResetPassword.txt", emailMessage, null);
+                    await _emailSender.SendEmailAsync(emailMessage.To, "Password reset", bodyHtml, bodyTxt);
+
+                    request.Handled = true;
+                    await session.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error processing password reset request");
+            }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stoppingToken"></param>
+        /// <returns></returns>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _store.Changes().ForDocumentsInCollection<PasswordResetRequest>()
-                .Subscribe(change => {
-                    SendPassword(change.Id).ConfigureAwait(false).GetAwaiter().GetResult();
-                });
+            using (var session = _store.OpenAsyncSession())
+            {
+                //TODO: Subscribe to chnages on TenantSettings and start listening to chnages on new databases
+
+                var tenantSettings = await session.Query<TenantSetting>().ToListAsync(stoppingToken);
+                foreach (var tenantSetting in tenantSettings)
+                {
+                    _store.Changes($"TenantIdentity-{tenantSetting.Identifier}").ForDocumentsInCollection<PasswordResetRequest>()
+                        .Subscribe(change => {
+                            if (change.Type == Raven.Client.Documents.Changes.DocumentChangeTypes.Put)
+                                SendPasswordAsync(change.Id, tenantSetting.Identifier).ConfigureAwait(false).GetAwaiter().GetResult();
+                        });
+                }
+            }
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                await Task.Delay(1000);
+                await Task.Delay(1000, stoppingToken);
             }
         }
     }
